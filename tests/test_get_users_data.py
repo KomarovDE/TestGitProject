40a600b478f6da2fce6f7691fb22ce6b1ce8f649
DK <komarovde94@mail.ru>
2024-04-02 11:38:18 +0300

    add new tests for gertting users data

---
+++ tests/test_get_users_data.py
@@ -0,0 +1,34 @@
+import httpx
+from jsonschema import validate
+from core.contracts import USER_DATA_SCHEMA
+
+BASE_URL = 'https://reqres.in/'
+LIST_USERS = 'api/users?page=2'
+EMAIL_ENDS = '@reqres.in'
+SINGLE_USER = 'api/users/2'
+USER_NOT_FOUND = 'api/users/23'
+
+def test_list_users():
+    response = httpx.get(BASE_URL + LIST_USERS)
+    assert response.status_code == 200  # проверка на верность условия
+
+    users_data = response.json()['data']
+    for item in users_data:
+        validate(item, USER_DATA_SCHEMA)
+        assert item['email'].endswith(EMAIL_ENDS)  # проверка email
+        assert item['avatar'] == f'{BASE_URL}img/faces/{item["id"]}-image.jpg'  # проверка avatar
+
+
+def single_user():
+    response = httpx.get(BASE_URL + LIST_USERS)
+    assert response.status_code == 200  # проверка на верность условия
+
+    users_data = response.json()['data']
+    validate(users_data, USER_DATA_SCHEMA)
+    assert users_data['email'].endswith(EMAIL_ENDS)  # проверка email
+    assert users_data['avatar'] == f'{BASE_URL}img/faces/{users_data["id"]}-image.jpg'  # проверка avatar
+
+def USER_NOT_FOUND():
+    response = httpx.get(BASE_URL + LIST_USERS)
+    assert response.status_code == 404  # проверка на верность условия
+
